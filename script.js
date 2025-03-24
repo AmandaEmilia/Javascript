f7ea8c4157ccd95308b711fb442cfa1c7d13039a
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-24 11:03:48 +0100

    declaring and calling method

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("This is a scipt!");
 
 alert("This is a pop-up!");
+
+const sayMyName = (name) => {
+  console.log("THis is your name: " + name);
+};
+
+sayMyName("Amanda");
+sayMyName("Eric");
